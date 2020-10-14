65579184613db18fb36cbcc2793cb550c26f4540
Blair-White <blairjwhite@hotmail.com>
2020-10-14 00:28:55 -0400

    up to keyboard input.

---
+++ Mobile_Lab_3/Assets/_Scripts/BackgroundController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public float verticalBound;
+    public float verticalSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Reset()
+    {
+        this.transform.position = new Vector3(0, verticalBound);
+    }
+    private void _Move()
+    {
+        transform.position -= new Vector3(0, verticalSpeed);
+
+    }
+
+    private void _CheckBounds()
+    {
+        //if the background is lower than the bottom of the screen, then reset.
+        if (transform.position.y <= -verticalBound) _Reset();
+
+    }
+}
---
+++ Mobile_Lab_3/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public float horizontalBound, verticalBound, hSpeed, maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Move()
+    {
+        float direction = 0.0f;
+
+        if(Input.GetAxis("Horizontal") >= 0.1f)
+        {
+            // dir = right
+            direction = 1.0f;
+
+        }
+
+        if(Input.GetAxis("Horizontal") <= -0.1f)
+        {
+            //dir = left
+            direction = -1.0f;
+        }
+
+        var newVelocity = rigidBody.velocity + new Vector2(direction * hSpeed, 0.0f);
+        rigidBody.velocity = Vector2.ClampMagnitude(newVelocity, maxSpeed);
+
+    }
+
+    private void _CheckBounds()
+    {
+        //CheckRightBounds
+        if (transform.position.x >= horizontalBound)
+        {
+            transform.position = new Vector3(horizontalBound, transform.position.y, 0.0f);
+        }
+        //Check LeftBounds
+        if (transform.position.x <= -horizontalBound)
+        {
+            transform.position = new Vector3(-horizontalBound, transform.position.y, 0.0f);
+        }
+    }
+}
