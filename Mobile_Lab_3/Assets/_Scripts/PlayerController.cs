178a493ff68c9ee57dae5f7f7a2b8113dd95348b
Blair-White <blairjwhite@hotmail.com>
2020-10-14 00:55:08 -0400

    Ended up copying touch input from lab4, as the lab took like 1hr to do touch input and its still not very good.

--- Mobile_Lab_3/Assets/_Scripts/PlayerController.cs
+++ Mobile_Lab_3/Assets/_Scripts/PlayerController.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Security.AccessControl;
+using System.ComponentModel;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rigidBody;
-    public float horizontalBound, verticalBound, hSpeed, maxSpeed;
+    //public BulletManager bulletManager;
+
+    [Header("Boundary Check")]
+    public float horizontalBoundary;
+
+    [Header("Player Speed")]
+    public float horizontalSpeed;
+    public float maxSpeed;
+    public float horizontalTValue;
+
+    [Header("Bullet Firing")]
+    public float fireDelay;
+
+    // Private variables
+    private Rigidbody2D m_rigidBody;
+    private Vector3 m_touchesEnded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_touchesEnded = new Vector3();
+        m_rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,41 +35,81 @@ public class PlayerController : MonoBehaviour
     {
         _Move();
         _CheckBounds();
+       // _FireBullet();
     }
+
+    //private void _FireBullet()
+    //{
+    //    // delay bullet firing 
+    //    if (Time.frameCount % 60 == 0 && bulletManager.HasBullets())
+    //    {
+    //        bulletManager.GetBullet(transform.position);
+    //    }
+    //}
 
     private void _Move()
     {
         float direction = 0.0f;
 
-        if(Input.GetAxis("Horizontal") >= 0.1f)
+        // touch input support
+        foreach (var touch in Input.touches)
         {
-            // dir = right
-            direction = 1.0f;
+            var worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (worldTouch.x > transform.position.x)
+            {
+                // direction is positive
+                direction = 1.0f;
+            }
+
+            if (worldTouch.x < transform.position.x)
+            {
+                // direction is negative
+                direction = -1.0f;
+            }
+
+            m_touchesEnded = worldTouch;
 
         }
 
-        if(Input.GetAxis("Horizontal") <= -0.1f)
+        // keyboard support
+        if (Input.GetAxis("Horizontal") >= 0.1f)
         {
-            //dir = left
+            // direction is positive
+            direction = 1.0f;
+        }
+
+        if (Input.GetAxis("Horizontal") <= -0.1f)
+        {
+            // direction is negative
             direction = -1.0f;
         }
 
-        var newVelocity = rigidBody.velocity + new Vector2(direction * hSpeed, 0.0f);
-        rigidBody.velocity = Vector2.ClampMagnitude(newVelocity, maxSpeed);
-
+        if (m_touchesEnded.x != 0.0f)
+        {
+            transform.position = new Vector2(Mathf.Lerp(transform.position.x, m_touchesEnded.x, horizontalTValue), transform.position.y);
+        }
+        else
+        {
+            Vector2 newVelocity = m_rigidBody.velocity + new Vector2(direction * horizontalSpeed, 0.0f);
+            m_rigidBody.velocity = Vector2.ClampMagnitude(newVelocity, maxSpeed);
+            m_rigidBody.velocity *= 0.99f;
+        }
     }
 
     private void _CheckBounds()
     {
-        //CheckRightBounds
-        if (transform.position.x >= horizontalBound)
+        // check right bounds
+        if (transform.position.x >= horizontalBoundary)
         {
-            transform.position = new Vector3(horizontalBound, transform.position.y, 0.0f);
+            transform.position = new Vector3(horizontalBoundary, transform.position.y, 0.0f);
         }
-        //Check LeftBounds
-        if (transform.position.x <= -horizontalBound)
+
+        // check left bounds
+        if (transform.position.x <= -horizontalBoundary)
         {
-            transform.position = new Vector3(-horizontalBound, transform.position.y, 0.0f);
+            transform.position = new Vector3(-horizontalBoundary, transform.position.y, 0.0f);
         }
+
     }
 }
